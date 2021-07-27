3093909c3873aec0209966ae8810f77a23321e68
PyroCalzone <84557296+PyroCalzone@users.noreply.github.com>
2021-07-27 00:21:26 -0500

    v2.0.0

--- src/app.js
+++ src/app.js
@@ -481,9 +481,12 @@ function packsToOptions(packs, pack_list, korm) {
       // app_logo.innerHTML = keycode;
       app_logo.classList.add('pressed');
 
+      const applicablekeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83]
+      const nonapplicablekeys = [57, 29, 3613, 42, 54, 58, 28, 15, 14, 56, 3640]
+
       // pack current pressed key
-      if(randomSounds){
-        current_key_down = Math.floor(Math.random() * 82)+1;
+      if(randomSounds && !nonapplicablekeys.includes(keycode)){
+        current_key_down = applicablekeys[Math.floor(Math.random() * applicablekeys.length)];
       }
       else{
         current_key_down = keycode
